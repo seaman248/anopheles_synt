08d3a0d622af7665e7e461f8863af5ae69265053
seaman248 <semen.bko@ya.ru>
2017-05-15 13:09:25 +0700

    mkdir chrs if not exsist

--- R/1_process_sp/process_alb.R
+++ R/1_process_sp/process_alb.R
@@ -15,6 +15,9 @@ albDNAStringSet <- albDNAStringSet[c('X', '2R', '2L', '3R', '3L')]
 
 names(albDNAStringSet) <- paste0('e', elements$alb)
 
+# prepare dir for chrs
+if(!dir.exists('./data/chrs')){dir.create('./data/chrs')}
+
 # save
 lapply(names(albDNAStringSet), function(chr){
   chrdir <- paste0('./data/chrs/', chr)
