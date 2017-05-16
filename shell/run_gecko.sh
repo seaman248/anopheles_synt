98a16edf8257c730a0f439c0bdd52b1cf66c7f1c
seaman248 <semen.bko@ya.ru>
2017-05-16 13:51:45 +0700

    change paths to files according to instance folder structure

--- shell/run_gecko.sh
+++ shell/run_gecko.sh
@@ -2,22 +2,30 @@
 
 elements=$(ls ./data/chrs)
 
-mkdir ./data/gecko_results
+mkdir -p ./data/gecko_results
+docker-machine ssh vmgecko mkdir -p ./anopheles_synt/data/gecko_results
 
 for el in $elements; do
 
-	mkdir './data/gecko_results/'$el
+	mkdir -p ./data/gecko_results/$el
+	docker-machine ssh vmgecko mkdir -p './anopheles_synt/data/gecko_results/'$el
 
 	docker run --rm \
-		-v "${PWD}:/anopheles_synt" \
-		-w '/anopheles_synt/data/gecko_results/'$el \
+		-v /home/docker-user/anopheles_synt/:/anopheles_synt \
+		-w /anopheles_synt/data/gecko_results/$el \
 		ubuntu:latest \
-		/anopheles_synt/gecko/bin/allVsAll.sh \
-			'/anopheles_synt/data/chrs/'$el \
-			200 65 32 fasta
+		/anopheles_synt/bin/allVsAll.sh \
+			/anopheles_synt/data/chrs/$el \
+			200 65 16 fasta
+
+	docker-machine ssh vmgecko rm -rf ./anopheles_synt/data/gecko_results/$el/intermediateFiles
+
+	docker-machine scp -r \
+		vmgecko:/home/docker-user/anopheles_synt/data/gecko_results/$el/results \
+		$PWD/data/gecko_results/$el/results
 
 done
-	
+
 
 
 
